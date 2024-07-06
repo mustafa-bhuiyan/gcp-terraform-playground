890d0eb67707f60a7a056e670714de346ba64739
mustafa-bhuiyan <mustafa5.iut@gmail.com>
2024-07-06 14:37:28 -0400

    google storage bucket creation and object upload

--- google_storage_bucket/main.tf
+++ google_storage_bucket/main.tf
@@ -1,5 +1,19 @@
 resource "google_storage_bucket" "gcp-tf-playground" {
-  name     = "gcp-tf-playground"
-  location = "US"
+  name          = "gcp-tf-playground"
+  location      = "US"
   force_destroy = true
+}
+
+resource "google_storage_bucket_object" "webp1" {
+  name       = "webp1"
+  source     = "1.webp"
+  bucket     = google_storage_bucket.gcp-tf-playground.name
+  depends_on = [google_storage_bucket.gcp-tf-playground]
+}
+
+resource "google_storage_bucket_object" "butterfly" {
+  name       = "butterfly"
+  source     = "butterfly.webp"
+  bucket     = google_storage_bucket.gcp-tf-playground.name
+  depends_on = [google_storage_bucket.gcp-tf-playground]
 }
--- google_storage_bucket/providers.tf
+++ google_storage_bucket/providers.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "google" {
   # Configuration options
-  project     = "sonic-trail-427517-r2"
-  region      = "us-central1"
-  zone        = "us-central1-a"
+  project = "sonic-trail-427517-r2"
+  region  = "us-central1"
+  zone    = "us-central1-a"
   //credentials = "../creds/keys.json"
 }
